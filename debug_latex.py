50c6fa215eb919d2d60cef314eddb4bcf6a5920b
YuQi-ZJ <492734658@qq.com>
2025-07-29 15:16:09 +0800

    增加参数

--- debug_latex.py
+++ debug_latex.py
@@ -14,24 +14,26 @@ def main():
     parser.add_argument(
         'directory_path',
         type=str,
-        default='/data/西子数据/CPS1000',  # 默认值
+        default='/data/西子数据/CPS1000',
         help='要处理的目录路径'
     )
 
-    # 添加布尔类型参数1
+    # 添加布尔类型参数1（默认True，可通过命令行设为False）
     parser.add_argument(
-        '--enable-feature1',
-        action='store_true',  # 当提供该参数时值变为True，否则为False
-        default=True,
-        help='启用功能1'
+        '--disable-feature1',  # 注意名称改为"disable"
+        dest='enable_feature1',  # 但目标变量名保持为enable_feature1
+        action='store_false',  # 当提供参数时存储False
+        default=True,  # 默认值为True
+        help='禁用功能1（默认启用）'
     )
 
-    # 添加布尔类型参数2
+    # 添加布尔类型参数2（默认True，可通过命令行设为False）
     parser.add_argument(
-        '--enable-feature2',
-        action='store_true',
+        '--disable-feature2',
+        dest='enable_feature2',
+        action='store_false',
         default=True,
-        help='启用功能2'
+        help='禁用功能2（默认启用）'
     )
 
     # 解析参数
