1a63b82ac14fd162cdc53bf527e01299f01bf56d
zhiling31 <2524258132@qq.com>
2025-08-06 14:41:05 +0800

    修改图片相似度算法

--- comparison_modules/image_comparison/SSIM_comparision.py
+++ comparison_modules/image_comparison/SSIM_comparision.py
@@ -9,37 +9,37 @@ import matplotlib.pyplot as plt
 logger = logging.getLogger("image_compare")
 
 
-def compare_images_SSIM(image_path1, image_path2,threshold=0.95,debug = False,result_output_path=""):
-    logger.debug(f"SSIM开始对比图片：{image_path1}和{image_path2}")
+def compare_images_SSIM(src_image_path, dst_image_path,threshold=0.95,debug = False,result_output_path=""):
+    logger.debug(f"SSIM开始对比图片：{src_image_path}和{dst_image_path}")
     # 1. 读取图片并统一尺寸
-    with open(image_path1, "rb") as f:
+    with open(src_image_path, "rb") as f:
         img_data = np.frombuffer(f.read(), dtype=np.uint8)
-        img1 = cv2.imdecode(img_data, cv2.IMREAD_COLOR)
-    with open(image_path2, "rb") as f:
+        src_image = cv2.imdecode(img_data, cv2.IMREAD_COLOR)
+    with open(dst_image_path, "rb") as f:
         img_data = np.frombuffer(f.read(), dtype=np.uint8)
-        img2 = cv2.imdecode(img_data, cv2.IMREAD_COLOR)    
-    if img1 is None or img2 is None:
-        logger.error(f"图片读取失败，请检查路径:{image_path1}和{image_path2}")
+        dst_image = cv2.imdecode(img_data, cv2.IMREAD_COLOR)    
+    if src_image is None or dst_image is None:
+        logger.error(f"图片读取失败，请检查路径:{src_image_path}和{dst_image_path}")
         raise ValueError("图片读取失败，请检查路径")
     
     # 自动调整至相同尺寸（取最小尺寸）
-    h, w = min(img1.shape[0], img2.shape[0]), min(img1.shape[1], img2.shape[1])
-    img1_resized = cv2.resize(img1, (w, h))
-    img2_resized = cv2.resize(img2, (w, h))
-    gray1 = cv2.cvtColor(img1_resized, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(img2_resized, cv2.COLOR_BGR2GRAY)
+    h, w = min(src_image.shape[0], dst_image.shape[0]), min(src_image.shape[1], dst_image.shape[1])
+    src_image_resized = cv2.resize(src_image, (w, h))
+    dst_image_resized = cv2.resize(dst_image, (w, h))
+    src_gray = cv2.cvtColor(src_image_resized, cv2.COLOR_BGR2GRAY)
+    dst_gray = cv2.cvtColor(dst_image_resized, cv2.COLOR_BGR2GRAY)
     if debug:
         # 3. 计算SSIM值和差异图
-        score, diff = ssim(gray1, gray2, full=True, win_size=11, data_range=255)
+        score, diff = ssim(src_gray, dst_gray, full=True, win_size=11, data_range=255)
         diff = (diff * 255).astype("uint8")  # 转换为0-255范围
         
         # 5. 可视化结果
         fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-        axes[0].imshow(cv2.cvtColor(img1_resized, cv2.COLOR_BGR2RGB))
+        axes[0].imshow(cv2.cvtColor(src_image_resized, cv2.COLOR_BGR2RGB))
         axes[0].set_title("Image 1")
         axes[0].axis('off')
         
-        axes[1].imshow(cv2.cvtColor(img2_resized, cv2.COLOR_BGR2RGB))
+        axes[1].imshow(cv2.cvtColor(dst_image_resized, cv2.COLOR_BGR2RGB))
         axes[1].set_title("Image 2")
         axes[1].axis('off')
         
@@ -49,12 +49,12 @@ def compare_images_SSIM(image_path1, image_path2,threshold=0.95,debug = False,re
         
         plt.tight_layout()
         plt.savefig(result_output_path, dpi=120)
-        plt.show()
+
     else:
         # 3. 计算SSIM值和差异图
-        score = ssim(gray1, gray2, full=False, win_size=11, data_range=255)
+        score = ssim(src_gray, dst_gray, full=False, win_size=11, data_range=255)
     is_similar = score >= threshold
     
-    logger.debug(f"SSIM对比结束：{image_path1}和{image_path2}，分数：{score},阈值:{threshold}")
+    logger.debug(f"SSIM对比结束：{src_image_path}和{dst_image_path}，分数：{score},阈值:{threshold}")
     return score, is_similar
 
--- comparison_modules/image_comparison/image_comparsion.py
+++ comparison_modules/image_comparison/image_comparsion.py
@@ -21,25 +21,25 @@ def init_child_process(lock):
 def compare_together_wrapper(args):
     """
     包装函数用于传递锁和矩阵坐标
-    args: (i, j, image1, image2, outputdir, lock)
+    args: (i, j, src_image, dst_image, outputdir, lock)
     """
-    i, j, image1, image2, outputdir, lock = args
+    i, j, src_image, dst_image, outputdir, lock = args
     try:
         # 执行原始对比逻辑
-        score = compare_together(image1, image2, outputdir, lock)
+        score = compare_together(src_image, dst_image, outputdir, lock)
         return (i, j, score)
     except Exception as e:
-        logger.error(f"对比失败: {image1} vs {image2}: {str(e)}{traceback.format_exc()}")
+        logger.error(f"对比失败: {src_image} vs {dst_image}: {str(e)}{traceback.format_exc()}")
         return (i, j, 0)  # 返回默认值
 
 # 原始对比函数（增加锁参数）
-def compare_together(image1, image2, outputdir, lock=None):
-    base1 = os.path.splitext(os.path.basename(image1))[0]
-    base2 = os.path.splitext(os.path.basename(image2))[0]
+def compare_together(src_image, dst_image, outputdir, lock=None):
+    base1 = os.path.splitext(os.path.basename(src_image))[0]
+    base2 = os.path.splitext(os.path.basename(dst_image))[0]
     
     # 执行图像对比算法
     try:
-        score_SSIM, is_similar_SSIM = compare_images_SSIM(image1, image2)
+        score_SSIM, is_similar_SSIM = compare_images_SSIM(src_image, dst_image)
 
         # 加锁写入结果
         log_entry = (
@@ -49,8 +49,8 @@ def compare_together(image1, image2, outputdir, lock=None):
 
     except Exception as e:
         tb_str = traceback.format_exc()
-        logger.error(f"{image1}和{image2}对比失败: {e}\n{tb_str}")
-        log_entry = f"{image1}和{image2}对比失败: {e}\n{tb_str}"
+        logger.error(f"{src_image}和{dst_image}对比失败: {e}\n{tb_str}")
+        log_entry = f"{src_image}和{dst_image}对比失败: {e}\n{tb_str}"
         score_SSIM,is_similar_SSIM = 0,False
     if lock:
         with lock:
@@ -69,21 +69,21 @@ def compare_image_list(image_dir1, image_dir2, outputdir, num_processes=4):
         image_exts = ('.jpg', '.jpeg', '.png')
         os.makedirs(outputdir, exist_ok=True)
         # 获取图像列表
-        image_list1 = sorted([
+        src_image_list = sorted([
             os.path.join(image_dir1, f) 
             for f in os.listdir(image_dir1) 
             if f.lower().endswith(image_exts)
         ])
-        image_list2 = sorted([
+        dst_image_list = sorted([
             os.path.join(image_dir2, f) 
             for f in os.listdir(image_dir2) 
             if f.lower().endswith(image_exts)
         ])
-        logger.info(f"文件夹下图片个数: {len(image_list1)} vs {len(image_list2)}")
+        logger.info(f"文件夹下图片个数: {len(src_image_list)} vs {len(dst_image_list)}")
         # 初始化共享资源
         manager = Manager()
         lock = manager.Lock()  # 跨进程文件锁
-        diff_matrix = manager.list([manager.list([0.0]*len(image_list2)) for _ in range(len(image_list1))])
+        diff_matrix = manager.list([manager.list([0.0]*len(dst_image_list)) for _ in range(len(src_image_list))])
         
         # 创建进程池
         myPool = Pool(
@@ -94,8 +94,8 @@ def compare_image_list(image_dir1, image_dir2, outputdir, num_processes=4):
         
         # 生成所有图像对任务
         tasks = []
-        for i, img1 in enumerate(image_list1):
-            for j, img2 in enumerate(image_list2):
+        for i, img1 in enumerate(src_image_list):
+            for j, img2 in enumerate(dst_image_list):
                 tasks.append((i, j, img1, img2, outputdir, lock))
         
         # 异步提交任务
@@ -114,38 +114,18 @@ def compare_image_list(image_dir1, image_dir2, outputdir, num_processes=4):
             diff_matrix[i][j] = score
         
         # 匹配相似图片对
-        same_pairs = []
-        #只取最高分
-        for i in range(len(image_list1)):
-            max_score = 0
-            max_j = -1
-            for j in range(len(image_list2)):
-                if diff_matrix[i][j] > max_score:
-                    max_score = diff_matrix[i][j]
-                    max_j = j
-                if diff_matrix[i][j]> 0 and max_score>0 and max_j!=j:
-                    logger.warning(f"图片{image_list1[i]}和多张图片相似度较高：{image_list2[max_j]}、{image_list2[j]}，(分数={max_score:.4f})")
-            if max_score > 0 and max_j != -1:
-                same_pairs.append((i, max_j))
-                logger.info(f"最佳匹配: {image_list1[i]} -> {image_list2[max_j]} (分数={max_score:.4f})")
-        # 取所有匹配结果为True的
-        # for i in range(len(image_list1)):
-        #     for j in range(len(image_list2)):
-        #         if diff_matrix[i][j] > 0:
-        #             same_pairs.append((i, j))
-        with open(os.path.join(outputdir, "compare_same_index.txt"), 'w', encoding='utf-8') as f:
-            json.dump(same_pairs, f,ensure_ascii=False)
-        with open(os.path.join(outputdir, "compare_image1.txt"), 'w', encoding='utf-8') as f:
-            json.dump(image_list1, f,ensure_ascii=False)
-        with open(os.path.join(outputdir, "compare_image2.txt"), 'w', encoding='utf-8') as f:
-            json.dump(image_list2, f,ensure_ascii=False)
+        same_groups = get_same_groups(diff_matrix, src_image_list, dst_image_list,outputdir)
+        with open(os.path.join(outputdir, "compare_src_image.txt"), 'w', encoding='utf-8') as f:
+            json.dump(src_image_list, f,ensure_ascii=False)
+        with open(os.path.join(outputdir, "compare_dst_image.txt"), 'w', encoding='utf-8') as f:
+            json.dump(dst_image_list, f,ensure_ascii=False)
 
         # 生成并保存映射关系
-        new_image1_map, new_image2_map = convert_token(image_list1, image_list2, same_pairs)
+        new_src_image_map, new_dst_image_map = convert_token(src_image_list, dst_image_list, same_groups)
         with open(os.path.join(outputdir, "compare_same_images.json"), 'w', encoding='utf-8') as f:
             json.dump({
-                Path(image_dir1).parts[-2]: new_image1_map,
-                Path(image_dir2).parts[-2]: new_image2_map
+                Path(image_dir1).parts[-2]: new_src_image_map,
+                Path(image_dir2).parts[-2]: new_dst_image_map
             }, f, indent=4, ensure_ascii=False)
         
         logger.info(f"完成对比: 共匹配{len(same_pairs)}对图像")
@@ -156,16 +136,88 @@ def compare_image_list(image_dir1, image_dir2, outputdir, num_processes=4):
         logger.error(f"对比失败: {e}\n{tb_str}")
         return False
 
+# 只取最高分
+def get_same_pairs(diff_matrix, src_image_list, dst_image_list,outputdir):
+    # 存储匹配结果的list
+    same_pairs = []
+    #只取最高分
+    for i in range(len(src_image_list)):
+        max_score = 0
+        max_j = -1
+        for j in range(len(dst_image_list)):
+            if diff_matrix[i][j] > max_score:
+                max_score = diff_matrix[i][j]
+                max_j = j
+            if diff_matrix[i][j]> 0 and max_score>0 and max_j!=j:
+                logger.warning(f"图片{src_image_list[i]}和多张图片相似度较高：{dst_image_list[max_j]}、{dst_image_list[j]}，(分数={max_score:.4f})")
+        if max_score > 0 and max_j != -1:
+            same_pairs.append((i, max_j))
+            logger.info(f"最佳匹配: {src_image_list[i]} -> {dst_image_list[max_j]} (分数={max_score:.4f})")
+    with open(os.path.join(outputdir, "compare_same_index.txt"), 'w', encoding='utf-8') as f:
+            json.dump(same_pairs, f,ensure_ascii=False)
+    return same_pairs
 
-def convert_token(image_list1,image_list2,same_pairs):
+# 只要相似度大于阈值就认为一样
+def get_same_groups(diff_matrix, src_image_list, dst_image_list,outputdir):
+    # 匹配相似图片对
+    same_groups = []
+    # 先找出所有匹配对
+    all_pairs = []
+    for i in range(len(src_image_list)):
+        for j in range(len(dst_image_list)):
+            if diff_matrix[i][j] > 0:
+                all_pairs.append((i, j))
+        
+    # 分组处理相同图片
+    while all_pairs:
+        current_i, current_j = all_pairs.pop(0)
+        group_i = {current_i}
+        group_j = {current_j}
+            
+        # 找出所有关联的i和j
+        changed = True
+        while changed:
+            changed = False
+            for i, j in all_pairs[:]:
+                if i in group_i or j in group_j:
+                    group_i.add(i)
+                    group_j.add(j)
+                    all_pairs.remove((i, j))
+                    changed = True
+            
+         # 转换为实际图片路径
+        same_groups.append({
+            "src_image": [src_image_list[i] for i in group_i],
+            "dst_image": [dst_image_list[j] for j in group_j]
+        })
+        logger.info(f"相同图片组: {group_i} -> {group_j}")
+        
+    # 保存结果
+    with open(os.path.join(outputdir, "compare_same_index.txt"), 'w', encoding='utf-8') as f:
+        json.dump(same_groups, f, ensure_ascii=False)
+    return same_groups
+
+
+def convert_token(src_image_list,dst_image_list,same_groups):
+    number = 276452563
+    new_src_image_map = {}
+    new_dst_image_map = {}
+    for group in same_groups:
+        new_src_image_map.update({os.path.basename(image): f"<image{number}>" for image in group["src_image"]})
+        new_dst_image_map.update({os.path.basename(image): f"<image{number}>" for image in group["dst_image"]})
+        number += 1
+    return new_src_image_map, new_dst_image_map
+
+# 只取最高分的same_pairs的token转化
+def convert_token_same_pairs(src_image_list,dst_image_list,same_pairs):
     number = 276352563
-    new_image1_map = {}
-    new_image2_map = {}
+    new_src_image_map = {}
+    new_dst_image_map = {}
     for pairs in same_pairs:
-        new_image1_map["![](figures/" + os.path.basename(image_list1[pairs[0]]) + ")"] = "<image" + str(number) + ">"
-        new_image2_map["![](figures/" + os.path.basename(image_list2[pairs[1]]) + ")"] = "<image" + str(number) + ">"
+        new_src_image_map["![](figures/" + os.path.basename(src_image_list[pairs[0]]) + ")"] = "<image" + str(number) + ">"
+        new_dst_image_map["![](figures/" + os.path.basename(dst_image_list[pairs[1]]) + ")"] = "<image" + str(number) + ">"
         number = number + 1
-    return new_image1_map,new_image2_map
+    return new_src_image_map,new_dst_image_map
 
 # 调用示例
 if __name__ == '__main__':
@@ -173,19 +225,19 @@ if __name__ == '__main__':
     # compare_image_list("D:/文件对比/Print_of_CPS1000_V_中英对照_内部.pdf/figures", "D:/文件对比/Print_of_CPS1000_W_中英对照_内部.pdf/figures", "D:/文件对比/图片对比结果", num_pools)
     with open("D:/文件对比/output/图片对比结果/compare_image1.txt", 'r', encoding='utf-8') as f:
         content = f.read().strip()  # 读取整行内容 → "['1', '2', 'apple', '3.5']"
-        image_list1 = json.loads(content) 
+        src_image_list = json.loads(content) 
     with open("D:/文件对比/output/图片对比结果/compare_image2.txt", 'r', encoding='utf-8') as f:
         content = f.read().strip()  # 读取整行内容 → "['1', '2', 'apple', '3.5']"
-        image_list2 = json.loads(content) 
+        dst_image_list = json.loads(content) 
     with open("D:/文件对比/output/图片对比结果/compare_same_index.txt", 'r', encoding='utf-8') as f:
         content = f.read().strip()  # 读取整行内容 → "['1', '2', 'apple', '3.5']"
         same_pairs = json.loads(content) 
-    convert_token(image_list1,image_list2,same_pairs)
-    new_image1_map, new_image2_map = convert_token(image_list1, image_list2, same_pairs)
+    convert_token(src_image_list,dst_image_list,same_pairs)
+    new_src_image_map, new_dst_image_map = convert_token(src_image_list, dst_image_list, same_pairs)
     image_dir1 = "CPS1000/Print_of_CPS1000_W_中英对照_内部.pdf/figures"
     image_dir2 = "CPS1000/Print_of_CPS1000_V_中英对照_内部.pdf/figures"
     with open(os.path.join("D:/文件对比/", "compare_same_images_new.json"), 'w', encoding='utf-8') as f:
         json.dump({
-            Path(image_dir1).parts[-2]: new_image1_map,
-            Path(image_dir2).parts[-2]: new_image2_map
+            Path(image_dir1).parts[-2]: new_src_image_map,
+            Path(image_dir2).parts[-2]: new_dst_image_map
         }, f, indent=4, ensure_ascii=False)
